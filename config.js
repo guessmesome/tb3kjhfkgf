d14c368cb49d0864abd02b71e82ce7c5f17e0cb7
nn <205666415+guessmesome@users.noreply.github.com>
2025-11-09 14:42:18 +0700

    upd

---
+++ config.js
@@ -0,0 +1,7 @@
+const productLinks = {
+    product1: "https://trnsfx.ru/go/s4uvd4q6d8",
+    product2: "https://trnsfx.ru/go/sngepkxfe8",
+    product3: "https://trnsfx.ru/go/s35slhhv9y",
+    product4: "https://trnsfx.ru/go/sky7o8aoug"
+};
+
