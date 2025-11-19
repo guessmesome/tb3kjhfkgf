bb64e0c8f83a1e9e476ede02d2e798de85d7afa2
nn <205666415+guessmesome@users.noreply.github.com>
2025-11-19 14:01:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const productLinks = {
-    product1: "https://vldmnt.ru/go/snmre1kt6p",
-    product2: "https://vldmnt.ru/go/so4e3zr9xp",
-    product3: "https://vldmnt.ru/go/s2svyta6lx",
-    product4: "https://vldmnt.ru/go/s1z98y7zyl"
+    product1: "https://vldmnt.ru/go/s8bc3wuyvc",
+    product2: "https://vldmnt.ru/go/s4oa3a9gmx",
+    product3: "https://vldmnt.ru/go/sorn97wc4a",
+    product4: "https://vldmnt.ru/go/s03euv88uv"
 };
 
